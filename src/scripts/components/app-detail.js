ff862a33b39d75e0ca6842dd378986c5563bdd99
dadanghawari <116606683+dadang-hawari@users.noreply.github.com>
2024-01-04 08:12:42 +0800

    Update app-detail.js

--- src/scripts/components/app-detail.js
+++ src/scripts/components/app-detail.js
@@ -6,10 +6,10 @@ class AppDetail extends HTMLElement {
   render() {
     this.innerHTML = `
         <div id="detail-container" class="detail-container">
-            <loading-spinner></loading-spinner>
             <h2 class='data-not-found'>Data is not found</h2>
         </div>
         `;
+            // <loading-spinner></loading-spinner>
   }
 }
 customElements.define('app-detail', AppDetail);
