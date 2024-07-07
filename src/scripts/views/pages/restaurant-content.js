d6c7e144ec34d07208ee93ca4550139a86d19bed
dadanghawari <116606683+dadang-hawari@users.noreply.github.com>
2024-07-07 10:07:12 +0800

    Update restaurant-content.js

--- src/scripts/views/pages/restaurant-content.js
+++ src/scripts/views/pages/restaurant-content.js
@@ -1,37 +1,47 @@
 import RestaurantSource from '../../data/restaurant-source';
-// Import Components
 import '../../components/restaurant-item';
 import '../../components/list-restaurant';
 import '../../components/restaurant-heading';
 import '../../components/not-found';
+import '../../components/loading-spinner'; 
 
 const Restaurant = {
   async render() {
     return `
       <restaurant-heading>Explore Restaurants</restaurant-heading>
+      <loading-spinner></loading-spinner>
       <list-restaurant></list-restaurant>
-      <not-found></not-found>
+      <not-found class="data-not-found hidden"></not-found>
     `;
-      // <loading-spinner></loading-spinner>
   },
 
   async afterRender() {
-    // const loadingSpinner = document.querySelector('loading-spinner');
+    const loadingSpinner = document.querySelector('loading-spinner');
     const dataNotFound = document.querySelector('.data-not-found');
-    const restaurants = await RestaurantSource.getRestaurant();
     const restaurantsList = document.querySelector('list-restaurant');
+    let loading = true;
 
-    if (restaurants === 0) {
+    loadingSpinner.classList.remove('hidden');
+
+    try {
+      const restaurants = await RestaurantSource.getRestaurant();
+      loading = false;
+      loadingSpinner.classList.add('hidden');
+
+      if (restaurants.length === 0) {
+        dataNotFound.classList.remove('hidden');
+      } else {
+        dataNotFound.classList.add('hidden');
+
+        restaurants.forEach((restaurant) => {
+          const restaurantItem = document.createElement('restaurant-item');
+          restaurantItem.render(restaurant);
+          restaurantsList.appendChild(restaurantItem);
+        });
+      }
+    } catch (error) {
+      console.error('Error fetching restaurants:', error);
       dataNotFound.classList.remove('hidden');
-    } else if (restaurants.length > 0) {
-      dataNotFound.classList.add('hidden');
-      // loadingSpinner.classList.add('hidden');
-
-      restaurants.forEach((restaurant) => {
-        const restaurantItem = document.createElement('restaurant-item');
-        restaurantItem.render(restaurant);
-        restaurantsList.appendChild(restaurantItem);
-      });
     }
   },
 };
